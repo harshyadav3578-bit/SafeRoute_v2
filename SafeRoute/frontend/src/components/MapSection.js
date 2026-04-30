866b5650a773c5406462112495cc4e03cf3ccb69
Harsh Yadav <harshyadav3578@gmail.com>
2026-04-30 07:45:56 +0530

    Fix frontend build warnings

--- SafeRoute/frontend/src/components/MapSection.js
+++ SafeRoute/frontend/src/components/MapSection.js
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   MapContainer,
   TileLayer,
-  Marker,
-  Popup,
   Polyline,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
